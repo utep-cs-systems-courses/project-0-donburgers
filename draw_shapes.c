0eb253e305c6ca86c9c5806d152310bf04b90120
student <bmata@miners.utep.edu>
2023-06-20 22:45:28 -0600

    modified:   draw.h 	modified:   draw_shapes.c 	modified:   uimain.c

--- draw_shapes.c
+++ draw_shapes.c
@@ -26,3 +26,10 @@ void print_triangle(int leftCol, int size)
   }
 }
 
+void print_arrow(int leftCol, int size) {
+
+  print_triangle(leftCol, size);
+  for(int i=0; i<(size/3); i++) {
+    print_square((leftCol*2)+2,(size/2));
+}
+}
